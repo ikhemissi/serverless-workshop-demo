56cf07de81a1b882ffabcb5a40d0677893495655
Iheb Khemissi <iheb.khemissi@gmail.com>
2023-04-14 16:45:12 +0200

    fix: Fix useEffect deps in NewTranscription
    Add `onFileUploaded` to the list of dependencies of `useEffect` in the `NewTranscription` component.
    
    noissue

--- src/NewTranscription.tsx
+++ src/NewTranscription.tsx
@@ -24,7 +24,7 @@ export default function NewTranscription({onFileUploaded}: NewTranscriptOptions)
     
             return () => controller.abort();
         }
-    }, [file]);
+    }, [file, onFileUploaded]);
 
     return (
         <AudioUploader onDropAccepted={files => setFile(files[0])} />
