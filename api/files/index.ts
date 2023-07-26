8439ffc53a2bfacee5780621a54c689f8fc71d76
Iheb Khemissi <iheb.khemissi@gmail.com>
2023-07-26 15:25:09 +0200

    feat: Support both binary and multipart uploads
    Add a new environment variable `FILE_UPLOADING_FORMAT` to allow specifying the file upload format expected by the upload endpoint:
    - `form` (default): multipart/form-data uploads
    - `binary`: send file contents directly in the POST request body
    
    noissue

--- api/files/index.ts
+++ api/files/index.ts
@@ -1,6 +1,19 @@
 import { Context, HttpRequest, HttpResponse } from "@azure/functions";
+import { getBoundary, parse } from 'parse-multipart-data';
 
-const fileUploadingUrl = process.env.FILE_UPLOADING_URL;
+const {FILE_UPLOADING_URL, FILE_UPLOADING_FORMAT} = process.env;
+
+// Data formats
+const binary = 'binary';
+const form = 'form';
+const supportedUploadFormats = [binary, form];
+
+const fileUploadingUrl = FILE_UPLOADING_URL;
+let fileUploadingFormat = FILE_UPLOADING_FORMAT;
+
+if (!supportedUploadFormats.includes(fileUploadingFormat)) {
+    fileUploadingFormat = form;
+}
 
 export default async function (context: Context, req: HttpRequest): Promise<HttpResponse> {
     try {
@@ -8,11 +21,23 @@ export default async function (context: Context, req: HttpRequest): Promise<Http
             throw new Error('Missing environment variable FILE_UPLOADING_URL');
         }
 
+        const originalContentType = req.headers['content-type'];
+        let contentType = originalContentType;
+        let body = req.bufferBody;
+
+        if (fileUploadingFormat === binary && originalContentType.includes('form-data')) {
+            const boundary = getBoundary(originalContentType);
+            const [file] = parse(req.bufferBody, boundary);
+
+            body = file.data;
+            contentType = file.type;
+        }
+
         const response = await fetch(fileUploadingUrl, {
             method: 'POST',
-            body: req.bufferBody,
+            body,
             headers: {
-                'Content-Type': req.headers['content-type'],
+                'Content-Type': contentType,
             },
         });
 
